b2aeb23eefc53927cc94dc79bb43bc044fff2d1e
Otávio Paganotti <pganottiotavio@gmail.com>
2021-08-04 15:08:15 -0400

    add: changes 2

--- src/entry-client.js
+++ src/entry-client.js
@@ -6,9 +6,9 @@ const { app, router, store } = createApp()
 
 sync(store, router);
 
-if (window.__INITIAL_STATE__) {
-  store.replaceState(window.__INITIAL_STATE__);
-}
+// if (window.__INITIAL_STATE__) {
+//   store.replaceState(window.__INITIAL_STATE__);
+// }
 
 // wait until router is ready before mounting to ensure hydration match
 router.isReady().then(() => {

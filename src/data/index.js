08b84d395f9364ba19de485eb3ab28728f997ca9
Otávio Paganotti <otaviopaganotti@outlook.com>
2022-06-22 07:50:21 -0400

    Implementa arquivo com lista de tecnologias

---
+++ src/data/index.js
@@ -0,0 +1,252 @@
+import imgHtml from '@/assets/icons/html5.svg';
+import imgCss from '@/assets/icons/css3-48.png';
+import imgJs from '@/assets/icons/javascript.svg';
+import imgTs from '@/assets/icons/typescript.svg';
+import imgVue from '@/assets/icons/vue-js-48.png';
+import imgQuasar from '@/assets/icons/Quasar_Logo.png';
+import imgSass from '@/assets/icons/sass.svg';
+import imgNode from '@/assets/icons/nodejs.svg';
+import imgCypress from '@/assets/icons/cypress.png';
+import imgExpress from '@/assets/icons/express.png';
+import imgPg from '@/assets/icons/postgresql.svg';
+import imgReact from '@/assets/icons/react.png';
+import imgNuxt from '@/assets/icons/Nuxt_logo.png';
+import imgVite from '@/assets/icons/vite.svg';
+import imgVitest from '@/assets/icons/vitest.svg';
+import imgDocker from '@/assets/icons/docker.svg';
+import imgBootstrap from '@/assets/icons/bootstrap.svg';
+import imgAdonis from '@/assets/icons/adonis.png';
+import imgTailwind from '@/assets/icons/tailwindcss.png';
+import imgMercadoPago from '@/assets/icons/mercadopago.png';
+import imgHoneybadger from '@/assets/icons/honeybadger.png';
+import imgWpp from '@/assets/icons/wpp.png';
+import imgSocketio from '@/assets/icons/socketio.png';
+import imgJava from '@/assets/icons/java.png';
+import imgRollup from '@/assets/icons/rollup.png';
+import imgWebpack from '@/assets/icons/webpack.png';
+import imgC from '@/assets/icons/c.png';
+import imgCpp from '@/assets/icons/cpp.png';
+import imgPhp from '@/assets/icons/php.png';
+import imgPy from '@/assets/icons/py.png';
+import imgIno from '@/assets/icons/ino.svg';
+import imgMaterialize from '@/assets/icons/materialize.png';
+import imgVuetify from '@/assets/icons/vuetify.svg';
+import imgCake from '@/assets/icons/cake.png';
+import imgJest from '@/assets/icons/jest.png';
+import imgMocha from '@/assets/icons/mocha.svg';
+
+export const html = {
+  url: 'https://www.w3schools.com/html/',
+  name: 'HTML 5',
+  image: imgHtml
+};
+
+export const css = {
+  url: 'https://www.w3schools.com/css/',
+  name: 'CSS 3',
+  image: imgCss
+};
+
+export const js = {
+  url: 'https://www.javascript.com/',
+  name: 'Javascript',
+  image: imgJs
+}
+
+export const vue = {
+  url: 'https://vuejs.org/',
+  name: 'Vue.Js',
+  image: imgVue
+};
+
+export const quasar = {
+  url: 'https://quasar.dev/',
+  name: 'Quasar Framework',
+  image: imgQuasar
+};
+
+export const sass = {
+  url: 'https://sass-lang.com/',
+  name: 'Sass',
+  image: imgSass
+};
+
+export const cy = {
+  url: 'https://www.cypress.io/',
+  name: 'Cypress',
+  image: imgCypress
+};
+
+export const node = {
+  url: 'https://nodejs.org/',
+  name: 'Node.Js',
+  image: imgNode
+};
+
+export const express = {
+  url: 'https://expressjs.com/',
+  name: 'Express .Js',
+  image: imgExpress
+};
+
+export const pg = {
+  url: 'https://www.postgresql.org/',
+  name: 'PostgreSQL',
+  image: imgPg
+};
+
+export const nuxt = {
+  url: 'https://nuxtjs.org/',
+  name: 'Nuxt Framework',
+  image: imgNuxt
+};
+
+export const react = {
+  url: 'https://reactjs.org/',
+  name: 'React.Js',
+  image: imgReact
+};
+
+export const bootstrap = {
+  url: 'https://getbootstrap.com/',
+  name: 'Bootstrap Framework',
+  image: imgBootstrap
+};
+
+export const ts = {
+  url: 'https://www.typescriptlang.org/',
+  name: 'Typescript',
+  image: imgTs
+};
+
+export const vite = {
+  url: 'https://vitejs.dev/',
+  name: 'Vite.Js',
+  image: imgVite
+};
+
+export const vitest = {
+  url: 'https://vitest.dev/',
+  name: 'Vitest',
+  image: imgVitest
+};
+
+export const tailwind = {
+  url: 'https://tailwindcss.com/',
+  name: 'Tailwind CSS',
+  image: imgTailwind
+};
+
+export const adonis = {
+  url: 'https://adonisjs.com/',
+  name: 'Adonis.Js',
+  image: imgAdonis
+};
+
+export const docker = {
+  url: 'https://www.docker.com/',
+  name: 'Docker',
+  image: imgDocker
+};
+
+export const mp = {
+  url: 'https://www.mercadopago.com.br/developers/en/reference',
+  name: 'Mercado Pago',
+  image: imgMercadoPago
+};
+
+export const honeybadger = {
+  url: 'https://www.honeybadger.io/',
+  name: 'Honeybadger',
+  image: imgHoneybadger
+};
+
+export const wpp = {
+  url: 'https://www.whatsapp.com/',
+  name: 'WhatsApp',
+  image: imgWpp
+};
+
+export const socketio = {
+  url: 'https://socket.io/',
+  name: 'Socket io',
+  image: imgSocketio
+};
+
+export const java = {
+  url: 'https://www.java.com/',
+  name: 'Java',
+  image: imgJava
+};
+
+export const rollup = {
+  url: 'https://rollupjs.org/guide/en/',
+  name: 'Rollup Js',
+  image: imgRollup
+};
+
+export const webpack = {
+  url: 'https://webpack.js.org/',
+  name: 'Webpack Js',
+  image: imgWebpack
+};
+
+export const c = {
+  url: 'https://www.w3schools.com/c/',
+  name: 'C Language Programing',
+  image: imgC
+};
+
+export const cpp = {
+  url: 'https://cplusplus.com/doc/tutorial/',
+  name: 'C++ Language Programing',
+  image: imgCpp
+};
+
+export const php = {
+  url: 'https://www.php.net/',
+  name: 'PHP Language Programing',
+  image: imgPhp
+};
+
+export const py = {
+  url: 'https://www.python.org/',
+  name: 'Python Language Programing',
+  image: imgPy
+};
+
+export const ino = {
+  url: 'https://www.arduino.cc/',
+  name: 'Arduino Language Programing',
+  image: imgIno
+};
+
+export const materialize = {
+  url: 'https://materializecss.com/',
+  name: 'Materialize CSS',
+  image: imgMaterialize
+};
+
+export const vuetify = {
+  url: 'https://vuetifyjs.com/en/',
+  name: 'Vuetify CSS',
+  image: imgVuetify
+};
+
+export const cake = {
+  url: 'https://cakephp.org/',
+  name: 'Cake PHP',
+  image: imgCake
+};
+
+export const jest = {
+  url: 'https://jestjs.io/',
+  name: 'Jest Test Runner',
+  image: imgJest
+};
+
+export const mocha = {
+  url: 'https://mochajs.org/',
+  name: 'Mocha Test Runner',
+  image: imgMocha
+};

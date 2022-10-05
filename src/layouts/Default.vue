8ea4b72e7a31e9f25b95b5f07f03a39909b4e6e0
Otávio Paganotti <otaviopaganotti@outlook.com>
2022-10-04 22:59:43 -0400

    refactor: all default layout to new things

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,45 +1,15 @@
 <template>
   <div>
-    <div class="sticky top-0 z-50 grid grid-cols-1 gap-4 m-4 xl:m-0 overflow-x-hidden bg-[#F8F8F8]">
-      <div>
-        <div class="max-w-screen-xl mx-auto">
-          <nav class="flex justify-between" aria-label="Global">
-            <div class="flex items-center justify-between w-full md:w-auto py-4">
-              <router-link v-ripple class="relative rounded-xl" @click="changeColor" to="/">
-                <logo class="w-auto h-16 shadow-neobrutalism" />
-              </router-link>
-              <div class="-mr-2 flex items-center md:hidden">
-              </div>
-            </div>
-            <div class="flex items-center">
-              <div class="ml-10 pr-4 space-x-3 md:space-x-8">
-                <router-link
-                  v-for="item in navigation"
-                  :key="item.name"
-                  :to="item.href"
-                  class="relative px-2 py-1 xl:px-4 xl:py-2 font-medium border rounded-xl border-black shadow-neobrutalism-min mt-1"
-                  :class="{
-                    'text-red-600': color === 'red',
-                    'text-yellow-600': color === 'yellow',
-                    'text-green-600': color === 'green',
-                    'text-blue-600': color === 'blue',
-                    'text-indigo-600': color === 'indigo',
-                    'text-purple-600': color === 'purple',
-                    'text-pink-600': color === 'pink',
-                  }"
-                  v-ripple
-                  :active-class="`rounded-xl text-white bg-${color}-600`"
-                >
-                  {{ item.name }}
-                </router-link>
-              </div>
-            </div>
-          </nav>
-        </div>
+    <div class="sticky flex top-0 z-50 gap-2 xl:m-0 overflow-x-hidden">
+      <div class="w-2/12 hidden lg:block"></div>
+      <div class="w-full lg:w-8/12">
+        <nav class="flex justify-start">
+          <img src="@/assets/images/logo.svg" />
+        </nav>
       </div>
     </div>
     <div v-if="showButton" class="fixed z-50 bottom-4 right-4 xl:bottom-8 xl:right-8">
-      <button v-ripple @click="handleClick" class="border border-black relative rounded-full w-10 h-10 shadow-neobrutalism-min flex justify-center items-center md:transition md:ease-in-out hover:-translate-y-1 hover:scale-110" :class="returnColor">
+      <button v-ripple @click="handleClick" class="bg-base-5 border-2 border-black relative w-10 h-10 shadow-neobrutalism-min flex justify-center items-center md:transition md:ease-in-out hover:-translate-y-1 hover:scale-110">
         <ChevronUpIcon class="text-white w-6" />
       </button>
     </div>
@@ -53,7 +23,6 @@
 
 <script>
 import { MenuIcon, XIcon, ChevronUpIcon } from '@heroicons/vue/outline'
-import { mapGetters } from 'vuex'
 import Logo from '../components/logo.vue'
 
 const navigation = [
@@ -71,40 +40,10 @@ export default {
 },
   data: () => ({
     navigation,
-    colors: [
-      'red',
-      'yellow',
-      'green',
-      'blue',
-      'indigo',
-      'purple',
-      'pink'
-    ],
     lastColor: null,
     showButton: false,
   }),
-  computed: {
-    ...mapGetters(['color']),
-    returnColor() {
-      return {
-        'bg-red-600': this.color === 'red',
-        'bg-yellow-600': this.color === 'yellow',
-        'bg-green-600': this.color === 'green',
-        'bg-blue-600': this.color === 'blue',
-        'bg-indigo-600': this.color === 'indigo',
-        'bg-purple-600': this.color === 'purple',
-        'bg-pink-600': this.color === 'pink',
-      }
-    }
-  },
   methods: {
-    changeColor () {
-      const newColor = this.colors[Math.floor(Math.random() * this.colors.length)]
-
-      if (newColor !== this.lastColor)
-        this.$store.commit('color', this.colors[Math.floor(Math.random() * this.colors.length)])
-      else this.changeColor()
-    },
     handleScroll() {
       const scrollTop = window.scrollY;
 
@@ -122,9 +61,6 @@ export default {
 
       this.changeColor();
     }
-  },
-  created () {
-    this.changeColor()
   },
   mounted() {
     document.addEventListener('scroll', this.handleScroll);

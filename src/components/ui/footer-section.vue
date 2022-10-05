35c7fdcb80b59fc754afeb4bbf4e321e62d16e86
Otávio Paganotti <otaviopaganotti@outlook.com>
2022-10-04 23:00:41 -0400

    add: create footer section

---
+++ src/components/ui/footer-section.vue
@@ -0,0 +1,27 @@
+<template>
+  <hr>
+  <footer class="p-4">
+    <div class="flex gap-12 justify-center">
+      <a
+        href="https://www.instagram.com/opag.software/"
+        target="_blank"
+        class="relative shadow-neobrutalism border-black border-2 bg-white transition ease-in-out delay-75 lg:hover:scale-[105%] flex gap-2"
+      >
+        <img src="@/assets/images/icons/instagram.svg" class="ml-2" />
+        <div class="px-4 py-2 font-semibold">
+          @opag.software
+        </div>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/otavio-paganotti/"
+        target="_blank"
+        class="relative shadow-neobrutalism border-black border-2 bg-white transition ease-in-out delay-75 lg:hover:scale-[105%] flex gap-2"
+      >
+        <img src="@/assets/images/icons/linkedin.svg" class="ml-2" />
+        <div class="px-4 py-2 font-semibold">
+          otavio-paganotti
+        </div>
+      </a>
+    </div>
+  </footer>
+</template>
